ec8248df1f5703ab84597097837bead90288512e
nichro02 <nichro02@gmail.com>
2021-09-21 19:28:23 -0600

    Wrote routes to get all todos, get todo by id, update todo, delete todo

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,34 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+## test server is responding
 @app.get('/')
 def read_root():
     return {'Hello world'}
+
+#ROUTES
+
+#get all todos route
+@app.get('/api/todo')
+async def get_todo():
+    return 1
+
+#get todo by id route
+@app.get('/api/todo{id}')
+async def get_todo_by_id(id):
+    return {'Return a specific todo'}
+
+#post todo route
+@app.post('/api/todo')
+async def post_todo(todo):
+    return {'POST todo'}
+
+#update todo route
+@app.put('/api/todo{id}')
+async def put_todo(id, data):
+    return {'UPDATE todo'}
+
+#delete todo route
+@app.delete('/api/todo{id}')
+async def delete_todo(id):
+    return {'DELETE todo'}
