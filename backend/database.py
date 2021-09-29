b92d424362b013d9b132ac60c7c44670e494729e
nichro02 <nichro02@gmail.com>
2021-09-28 19:50:34 -0600

    route CRUD routes

--- backend/database.py
+++ backend/database.py
@@ -10,3 +10,42 @@ client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
 #create database
 database = client.TodoList
 collection = database.todo
+
+#fetch single todo
+async def fetch_one_todo(title):
+    document = await collection.find_one({'title': title})
+
+    return document
+
+#fetch all todos
+async def fetch_all_todos():
+    todos = []
+    cursor = collection.find({})
+
+    #loop over documents in collection
+    async for document in cursor:
+        todos.append(Todo(**document))
+    
+    return todos
+
+#create a todo
+async def create_todo(todo):
+    document = todo
+    result = await collection.insert_one(document)
+    return document
+
+#update todo
+async def update_todo(title, desc):
+    await collection.update_one({'title': title}, {'$set':{
+        'description': desc
+    }})
+
+    document = await collection.find_one({'title': title})
+
+    return document
+
+#delete todo
+async def remove_todo(title):
+    await collection.delete_one({'title': title})
+
+    return True
