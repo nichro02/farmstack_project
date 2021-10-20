c14d479802b6a1a3b790541b87704b817dfb911d
nichro02 <nichro02@gmail.com>
2021-10-19 19:24:08 -0600

    Modified styling in App.cssand set up structure for App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from 'react'
+import axios from 'axios'
+
+//import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       Hello, world! This is FARM stack
+
+      <div className="TodoList list-group-item justify-content-center align-items-center mx-auto">
+        Todos go here
+      </div>
     </div>
   );
 }
