72a177d70893994c22c77e627372953c62f7ce4c
nichro02 <nichro02@gmail.com>
2021-10-27 17:45:00 -0600

    Structured task manager page so users will be able to add tasks and view existing tasks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,20 @@ function App() {
       Hello, world! This is FARM stack
 
       <div className="TodoList list-group-item justify-content-center align-items-center mx-auto">
-        Todos go here
+        <h1 className='card text-white bg-primary mb-1' styleName='max-width: 20 rem'>Task Manager</h1>
+        <h6 className='card text-white bg-primary mb-3'>FASTAPI - React - MongoDB</h6>
+        <div className='card-body'></div>
+        <h5 className='card text-white bg-dark mb-3'>Add Your Task Below</h5>
+        <span className='card-text'>
+          <input className='mb-2 form-control titleIn' placeholder='Title'/>
+          <input className='mb-2 form-control desIn' placeholder='Description'/>
+          <button className="btn btn-outline-primary mx-2 mb-3">
+            Save Task
+          </button>
+        </span>
+      </div>
+      <div className='TodoList list-group-item justify-content-center align-items-center mx-auto mt-5 border-0'>
+        <h5 className='card text-white bg-dark mb-3'>Your Tasks</h5>
       </div>
     </div>
   );
